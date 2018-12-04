1151303b92b11cde14b07494562b74ddbcc4e537
pedromigliatti <pedro.migliatti@gmail.com>
2018-12-04 14:58:49 -0200

    Melhoria na apresentação dos ingredientes

--- app/src/main/java/com/example/pedro/drinks/scenarios_main/DrinkActivity.kt
+++ app/src/main/java/com/example/pedro/drinks/scenarios_main/DrinkActivity.kt
@@ -2,6 +2,8 @@ package com.example.pedro.drinks.scenarios_main
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import com.example.pedro.drinks.entities.Drink
 import com.example.pedro.drinks.utils.GlideApp
 import com.example.pedro.drinks.R
@@ -27,50 +29,22 @@ class DrinkActivity : AppCompatActivity() {
         categoria.text = drink.strCategory
         glass.text = drink.strGlass
         instructions.text = drink.strInstructions
-        measure1.text = drink.strMeasure1
-        ingre1.text = drink.strIngredient1
 
-        measure2.text = drink.strMeasure2
-        ingre2.text = drink.strIngredient2
-
-        measure3.text = drink.strMeasure3
-        ingre3.text = drink.strIngredient3
-
-        measure4.text = drink.strMeasure4
-        ingre4.text = drink.strIngredient4
-
-        measure4.text = drink.strMeasure4
-        ingre4.text = drink.strIngredient4
-
-        measure5.text = drink.strMeasure5
-        ingre5.text = drink.strIngredient5
-
-        measure6.text = drink.strMeasure6
-        ingre6.text = drink.strIngredient6
-
-        measure7.text = drink.strMeasure7
-        ingre7.text = drink.strIngredient7
-
-        measure8.text = drink.strMeasure9
-        ingre8.text = drink.strIngredient9
-
-        measure10.text = drink.strMeasure10
-        ingre10.text = drink.strIngredient10
-
-        measure11.text = drink.strMeasure11
-        ingre11.text = drink.strIngredient11
-
-        measure12.text = drink.strMeasure12
-        ingre12.text = drink.strIngredient12
-
-        measure13.text = drink.strMeasure13
-        ingre13.text = drink.strIngredient13
-
-        measure14.text = drink.strMeasure14
-        ingre14.text = drink.strIngredient14
-
-        measure15.text = drink.strMeasure15
-        ingre15.text = drink.strIngredient15
+        showTextView(measure1, ingre1, drink)
+        showTextView(measure2, ingre2, drink)
+        showTextView(measure3, ingre3, drink)
+        showTextView(measure4, ingre4, drink)
+        showTextView(measure5, ingre5, drink)
+        showTextView(measure6, ingre6, drink)
+        showTextView(measure7, ingre7, drink)
+        showTextView(measure8, ingre8, drink)
+        showTextView(measure9, ingre9, drink)
+        showTextView(measure10, ingre10, drink)
+        showTextView(measure11, ingre11, drink)
+        showTextView(measure12, ingre12, drink)
+        showTextView(measure13, ingre13, drink)
+        showTextView(measure14, ingre14, drink)
+        showTextView(measure15, ingre15, drink)
 
         GlideApp.with(this)
                 .load(drink.strDrinkThumb)
@@ -79,4 +53,18 @@ class DrinkActivity : AppCompatActivity() {
                 .into(imgFoto)
         
     }
+
+    private fun showTextView(measure: TextView, ingre: TextView, drink: Drink){
+        if(ingre.text.isEmpty()){
+            ingre.visibility = View.GONE
+            measure.visibility = View.GONE
+        } else {
+            ingre.visibility = View.VISIBLE
+            measure.visibility = View.VISIBLE
+            measure.text = drink.strMeasure1
+            ingre.text = drink.strIngredient1
+        }
+    }
 }
+
+
